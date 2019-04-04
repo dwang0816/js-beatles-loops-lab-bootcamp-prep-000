c0c830b83eb75da96e8f2033f3c1d8cba50c8500
dennis wang <dwang0816@gmail.com>
2019-04-04 22:26:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var beatles = new Array();
+  for (let i = 0; i < musicians.length; i++){
+    beatles.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return beatles;
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  var exclamationArray = new Array();
+  while (i < facts.length) {
+    exclamationArray.push(facts[i] + "!!!")
+    i++;
+  }
+  return exclamationArray;
+}
